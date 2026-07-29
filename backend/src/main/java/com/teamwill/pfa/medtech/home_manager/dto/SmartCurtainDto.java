6624fc717bf8f3cc10d2cc03fc9e5a6d1fa3ff57
Malik251204 <darrajimalik6@gmail.com>
2026-07-29 16:39:10 +0100

    Update SmartCurtainDto.java

--- backend/src/main/java/com/teamwill/pfa/medtech/home_manager/dto/SmartCurtainDto.java
+++ backend/src/main/java/com/teamwill/pfa/medtech/home_manager/dto/SmartCurtainDto.java
@@ -1,6 +1,5 @@
 package com.teamwill.pfa.medtech.home_manager.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,7 +8,6 @@ import lombok.experimental.SuperBuilder;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @SuperBuilder
 public class SmartCurtainDto extends DeviceDto {
 }
