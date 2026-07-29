412adae41adae535af361c0f53e2206908a2fbb0
Malik Darraji <darrajimalik6@gmail.com>
2026-07-29 15:10:57 +0100

    ahbed

---
+++ backend/src/test/java/com/teamwill/pfa/medtech/home_manager/HomeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamwill.pfa.medtech.home_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
