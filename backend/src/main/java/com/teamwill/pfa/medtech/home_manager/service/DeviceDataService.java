e18e08d4ac8c97104ae1faf0ac4c6012cadc8865
Malik251204 <darrajimalik6@gmail.com>
2026-07-30 17:10:45 +0100

    Simplify DeviceDataService to avoid Jackson dependency

--- backend/src/main/java/com/teamwill/pfa/medtech/home_manager/service/DeviceDataService.java
+++ backend/src/main/java/com/teamwill/pfa/medtech/home_manager/service/DeviceDataService.java
@@ -1,13 +1,12 @@
 package com.teamwill.pfa.medtech.home_manager.service;
 
 import com.teamwill.pfa.medtech.home_manager.dto.CommandDto;
-import com.teamwill.pfa.medtech.home_manager.dto.DeviceDataRequest;
 import com.teamwill.pfa.medtech.home_manager.dto.QueueCommandRequest;
 
 import java.util.List;
 
 public interface DeviceDataService {
-    void receiveData(Long deviceId, DeviceDataRequest request);
+    void receiveData(Long deviceId, String rawReadingsJson);
     List<CommandDto> getPendingCommands(Long deviceId);
     void acknowledgeCommand(Long deviceId, Long commandId, String status);
     CommandDto queueCommand(Long deviceId, QueueCommandRequest request);
